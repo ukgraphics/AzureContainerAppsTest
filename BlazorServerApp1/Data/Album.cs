2f994545dfd5925405ff055381b19954da9b322c
Kuni <kuni.ujiie@grapecity.com>
2022-07-08 11:12:08 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorServerApp1/Data/Album.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorServerApp1.Data
+{
+    public class Album
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+
+        [JsonPropertyName("artist")]
+        public string? Artist { get; set; }
+
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+
+        [JsonPropertyName("image_url")]
+        public string? Image_url { get; set; }
+
+    }
+}
